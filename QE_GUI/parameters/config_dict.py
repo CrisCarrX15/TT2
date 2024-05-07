f57c182082f31586ed2ab193ab50b998fea018f9
CrisCarrX15 <92184223+CrisCarrX15@users.noreply.github.com>
2024-05-06 22:38:07 -0600

    Add files via upload

---
+++ QE_GUI/parameters/config_dict.py
@@ -0,0 +1,12 @@
+CONFIG_DICT = { 
+        'performance': {
+            'input_type': 'select_multiple',
+            'options': ['High', 
+                        'Standard'],
+            },
+        '3D_graph': {
+            'input_type': 'select_multiple',
+            'options': ['Yes', 
+                        'No'],
+        }
+}
